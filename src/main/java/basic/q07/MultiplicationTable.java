2515492068097b1d0b46ca9e51ca6dd7edbf28c0
高田旭 <as-takada@seassist.co.jp>
2024-08-24 15:43:29 +0900

    basic commit

---
+++ src/main/java/basic/q07/MultiplicationTable.java
@@ -0,0 +1,14 @@
+package basic.q07;
+
+public class MultiplicationTable {
+
+	public static void main(String[] args) {
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 9; j++) {
+                System.out.printf("%2d ", i * j);
+            }
+            System.out.println();
+        }
+    }
+
+}
